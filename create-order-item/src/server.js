7e855f14b19f1d8630afc1c7ffde36885bbd4724
somesh202 <somesh.ranjan.met20@itbhu.ac.in>
2024-02-03 20:11:08 +0530

    Minor fi

--- create-order-item/src/server.js
+++ create-order-item/src/server.js
@@ -49,43 +49,60 @@ const CREATE_USER_ORDER = `mutation createUserOrder($city: String!, $country: St
 }`;
 
 const execute = async (variables, operation, reqHeaders) => {
-  const fetchResponse = await fetch(
-    "<your_GraphQL_endpoint",
-    {
-      method: 'POST',
-      headers: {
+  const fetchResponse = await fetch("<your_GraphQL_endpoint", {
+    method: "POST",
+    headers:
+      {
         ...reqHeaders,
-        'x-hasura-access-key': process.env.HASURA_GRAPHQL_ADMIN_SECRET
+        "x-hasura-access-key": process.env.HASURA_GRAPHQL_ADMIN_SECRET,
       } || {},
-      body: JSON.stringify({
-        query: operation,
-        variables
-      })
-    }
-  );
+    body: JSON.stringify({
+      query: operation,
+      variables,
+    }),
+  });
 
   const data = await fetchResponse.json();
   return data;
 };
 
-app.post('/createOrderItem', async (req, res) => {
+app.post("/createOrderItem", async (req, res) => {
   const { product_id, quantity } = req.body.input;
-  const user_id = req.body.session_variables['x-hasura-user-id'];
-  let latest_order = '';
+  const user_id = req.body.session_variables["x-hasura-user-id"];
+  let latest_order = "";
 
   // check if the user has any orders
-  const { data: orderExists, errors: orderExistsErr } = await execute({ user_id }, USER_ORDER_EXISTS, req.body.session_variables);
+  const { data: orderExists, errors: orderExistsErr } = await execute(
+    { user_id },
+    USER_ORDER_EXISTS,
+    req.body.session_variables
+  );
 
   if (orderExistsErr) {
     return res.status(400).json({ error: orderExistsErr[0].message });
   }
 
-  // if the user doesn't have any orders or the latest order is ordered 
+  // if the user doesn't have any orders or the latest order is ordered
   //   create a new order and store its ID in "latest_order"
   // otherwise, store the ID of the latest order in "latest_order"
-  if (orderExists.users[0].orders.length === 0 || orderExists.users[0].orders[0].ordered === true) {
-    const { data: createOrder, errors: createOrderErr } = await execute({ city: '', country: '', house_number: 0, street_name: '', user_id, zip_code: '', total: 0}, CREATE_USER_ORDER, req.body.session_variables);
-    
+  if (
+    orderExists.users[0].orders.length === 0 ||
+    orderExists.users[0].orders[0].ordered === true
+  ) {
+    const { data: createOrder, errors: createOrderErr } = await execute(
+      {
+        city: "",
+        country: "",
+        house_number: 0,
+        street_name: "",
+        user_id,
+        zip_code: "",
+        total: 0,
+      },
+      CREATE_USER_ORDER,
+      req.body.session_variables
+    );
+
     if (createOrderErr) {
       return res.status(400).json({ error: createOrderErr[0].message });
     }
@@ -96,15 +113,19 @@ app.post('/createOrderItem', async (req, res) => {
   }
 
   // create the order item and add it to the order
-  const { data: createOrder, errors: createOrderErr } = await execute({ product_id, quantity, order_id: latest_order }, CREATE_ORDER_ITEM, req.body.session_variables);
+  const { data: createOrder, errors: createOrderErr } = await execute(
+    { product_id, quantity, order_id: latest_order },
+    CREATE_ORDER_ITEM,
+    req.body.session_variables
+  );
 
   if (createOrderErr) {
-    return res.status(400).json({ error: createOrderErr[0].message});
+    return res.status(400).json({ error: createOrderErr[0].message });
   }
 
   // success
   return res.json({
-    ...createOrder.insert_order_item_one
+    ...createOrder.insert_order_item_one,
   });
 });
 
